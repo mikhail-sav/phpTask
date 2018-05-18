5da84ca602636f4eacd97d928391516aeeb1f623
Misha Savelev <mikhailsav1997@gmail.com>
2018-05-18 09:15:04 +0300

    specification

--- index.php
+++ index.php
@@ -19,6 +19,9 @@
     
 
     <?php
+        // ПРОГРАММА НАХОДИТ СЛОВА, ВВЕДЕНЫЕ В ПОСКОВУЮ СТРОКУ, ПОЛЬЗОВАТЕЛЕМ.
+        // КАЖДОЕ СЛОВО ИЩЕТСЯ ИДНИВИДУАЛЬНО.
+        // ЕСЛИ СЛОВОСОЧЕТАНИЕ НАХОДИТСЯ В КАВЫЧКАХ, ПО ОНО ИЩЕТСЯ, КАК ЕДИНСТВЕННОЕ ВХОЖДЕНИЕ
         $string1 = "Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque
                     laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi 
                     architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit,
